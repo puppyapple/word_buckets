ef3550aa7a2f65da3b7e6303143d4efac9626f58
puppyapple <puppyapplewzj@126.com>
2018-06-27 10:47:18 +0800

    sdf

--- Code/data_preparation.py
+++ Code/data_preparation.py
@@ -1,17 +1,52 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import configparser
+import pymysql
 import pickle
+import jieba
+import jieba.analyse
 
-def comp_tag(data_raw_new, data_raw_old_full, nctag_filter_num=15):
+config = configparser.ConfigParser()
+config.read("../Data/Input/database_config/database.conf")
+host = config['ASSESSMENT']['host']
+user = config['ASSESSMENT']['user']
+password = config['ASSESSMENT']['password']
+database = config['ASSESSMENT']['database']
+port = config['ASSESSMENT']['port']
+charset = config['ASSESSMENT']['charset']
+db = pymysql.connect(host=host, user=user, password=password, db=database, port=int(port), charset=charset)
+config.read("../Data/Input/Tag_graph/filter.conf")
+keep_list = config['FILTER']['filter_list'].split(",")
+
+def comp_tag(new_result="company_tag_info_latest", old_result="company_tag", data_intro="company_intro_info_latest", label_code_relation="label_code_relation", keep_list=keep_list, db=db, nctag_filter_num=15):
+    
+    # 从库中读取数据
+    sql_new_result = "select * from %s" % new_result
+    sql_old_result = "select * from %s" % old_result
+    sql_intro = "select * from %s" % data_intro
+    sql_label_code_relation = "select * from %s" % label_code_relation
+    
+    data_raw_new = pd.read_sql(sql_new_result, con=db)
+    data_raw_old_full = pd.read_sql(sql_old_result, con=db)
+    data_intro = pd.read_sql(sql_intro, con=db)
+    label_chains_raw = pd.read_sql(sql_label_code_relation, con=db)
+    
+    # 全部概念标签的列表
+    ctag_full_list = set(label_chains_raw.label_note_name).union(set(label_chains_raw.label_root_name))
+    
     # 根据输入的公司概念和非概念标记源数据，分别得到完整的公司-概念标签、公司-非概念标签
-    data_raw_new = data_raw_new[data_raw_new.remarks != "1"].copy()
     data_raw_new.dropna(subset=["comp_id", "label_name"], inplace=True)
-    cols = ["comp_id", "comp_full_name", "label_name", "classify_id", "label_type", "label_type_num", "src_tags"]
+    cols = ["comp_id", "comp_full_name", "label_name", "classify_id", "label_type", "label_type_num", "src_tags", "remarks"]
     data_raw_new = data_raw_new[data_raw_new.label_name != ''][cols].copy()
 
-    # comp_ctag_table_all_infos 是读取文件后带概念标签的全部信息
+    # 将技术标签对应的标签作处理，在标签前加上前缀以示区别（和非概念标签）
+    data_raw_new.label_name = data_raw_new[["label_name", "remarks"]].apply(lambda x: "技术标签_" + x[0] if x[1] == "1" and x[0] not in ctag_full_list else x[0], axis=1)
+    
+    # 过滤掉进行中的产业链话题等关联的公司
     comp_ctag_table_all_infos = data_raw_new[data_raw_new.classify_id != 4].reset_index(drop=True)
+    comp_ctag_table_all_infos.src_tags = comp_ctag_table_all_infos[["label_type_num", "src_tags"]].apply(lambda x: x[1].split("#")[x[0] - 1], axis=1)
+    comp_ctag_table_all_infos = comp_ctag_table_all_infos[comp_ctag_table_all_infos.src_tags.apply(lambda x: x.split("-")[0]).isin(keep_list)]
+    # print(set(comp_ctag_table_all_infos.src_tags.apply(lambda x: x.split("-")[0])))
     comp_ctag_table = comp_ctag_table_all_infos[["comp_id", "label_name"]].reset_index(drop=True)
     
     # 新系统下结果中的公司-非概念标签
@@ -32,8 +67,8 @@ def comp_tag(data_raw_new, data_raw_old_full, nctag_filter_num=15):
     flatted = [y for x in tuples for y in x]
     data_raw_nctag_flatted = pd.DataFrame(flatted, columns=["comp_id", "label_name"]).drop_duplicates()
     
-    # 去掉已经是概念标签的记录作为公司-非概念标签的全集
-    comp_nctag_table = data_raw_nctag_flatted[~data_raw_nctag_flatted.label_name.isin(comp_ctag_table_all_infos.label_name.drop_duplicates())].copy()
+    # 取没有概念标记的作为非概念标签的全集
+    comp_nctag_table = data_raw_nctag_flatted[~data_raw_nctag_flatted.label_name.isin(ctag_full_list)].reset_index(drop=True)
     
     # 合并成公司-标签的一一对应集合
     comp_ctag_table["type"] = "ctag"
@@ -53,21 +88,23 @@ def comp_tag(data_raw_new, data_raw_old_full, nctag_filter_num=15):
     pickle.dump(comp_id_name_dict, comp_id_name_dict_file)
     comp_id_name_dict_file.close()
     
-    return (comp_tag_merged, comp_ctag_table_all_infos)
+    comp_intro_merged = data_intro.groupby("comp_id").agg({"intro": lambda x: "。".join(set(x))}).reset_index() 
+    return (comp_tag_merged, comp_intro_merged, comp_ctag_table_all_infos)
 
 
 def link_cutter(link_str, sep):
     pieces = link_str.split(sep)
     return [(sep.join(pieces[:i]), i) for i in range(1, len(pieces) + 1)]
     
-def comp_ctaglinks(comp_ctag_table_all_infos):
+def comp_ctaglinks(comp_ctag_table_all_infos_raw):
     # 公司和其命中的概念标签链
-    to_filter = ["企业云服务", "企业大数据解决方案", "机器人本体", "行业大数据解决方案"]
-    comp_ctag_table_all_infos.src_tags = comp_ctag_table_all_infos[["label_type_num", "src_tags"]].apply(lambda x: x[1].split("#")[x[0] - 1], axis=1)
-    comp_ctag_table_all_infos_ = comp_ctag_table_all_infos[~comp_ctag_table_all_infos.src_tags.apply(lambda x: x.split("-")[0]).isin(to_filter)]
+    # to_filter = ["企业云服务", "企业大数据解决方案", "机器人本体", "行业大数据解决方案"]
+    comp_ctag_table_all_infos = comp_ctag_table_all_infos_raw.copy()
+    # comp_ctag_table_all_infos.src_tags = comp_ctag_table_all_infos[["label_type_num", "src_tags"]].apply(lambda x: x[1].split("#")[x[0] - 1], axis=1)
+    # comp_ctag_table_all_infos_ = comp_ctag_table_all_infos[comp_ctag_table_all_infos.src_tags.apply(lambda x: x.split("-")[0]).isin(keep_list)]
     comp_ctag_fulllink = comp_ctag_table_all_infos[["comp_id", "src_tags"]].drop_duplicates()
     comp_ctag_fulllink["link_pieces"] = comp_ctag_fulllink.src_tags.apply(lambda x: link_cutter(x, "-"))
+    # print(set(comp_ctag_table_all_infos.src_tags))
     to_flatten = comp_ctag_fulllink.apply(lambda x: [(x[0], t[0], t[1]) for t in x[2]], axis=1)
     comp_ctaglinks = pd.DataFrame([y for x in to_flatten for y in x], columns=["comp_id", "link", "level"]).drop_duplicates()
     return comp_ctaglinks
-
--- Code/word_bucket.py
+++ Code/word_bucket.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import numpy as np
 import pickle
+import jieba
+import jieba.analyse
+import multiprocessing as mp
 from sklearn.preprocessing import MinMaxScaler
 
 def simple_minmax(df, col, min_v=0, max_v=1):
@@ -23,7 +26,7 @@ def bucket_by_tag(bucket_level, comp_ctaglinks, comp_tag_merged):
     
     tf_each_link = comp_link_with_all_tags.groupby(["link", "label_name"]).agg({"type":"count"}).groupby(level=0).apply(lambda x: x/float(x.sum())).reset_index(level=1).rename(index=str, columns={"type": "tf"})
     tf_each_link["idf"] = tf_each_link.label_name.apply(lambda x: idf_dict.get(x, 0))
-    tf_each_link["link_value"] = (tf_each_link.tf * tf_each_link.idf).apply(lambda x: np.log2(x))
+    tf_each_link["link_value"] = (tf_each_link.tf * tf_each_link.idf) #.apply(lambda x: np.log2(x))
     
     result_by_link = tf_each_link.groupby("link")
     link_list = list(result_by_link.groups.keys())
@@ -33,13 +36,77 @@ def bucket_by_tag(bucket_level, comp_ctaglinks, comp_tag_merged):
     tag_bucket_file = open(tag_bucket_file_name, "wb")
     pickle.dump(link_result_dict, tag_bucket_file)
     tag_bucket_file.close()
+    print("###tag_bucket_lv%d.pkl generated!###" % bucket_level)
     return link_result_dict
 
 
 
 # 基于冠文本的统计，可以同时使用TF-IDF和Text_rank（jieba接口）
-def bucket_by_text():
+def bucket_by_text(bucket_level, comp_ctaglinks, comp_intro_merged):
+    level_groups = comp_ctaglinks.groupby("level")
+    comp_link = level_groups.get_group(bucket_level)
+    comp_link_with_all_intros = comp_link.merge(comp_intro_merged, how="left", left_on="comp_id", right_on="comp_id")
+    intro_merged_each_link = comp_link_with_all_intros.groupby("link").agg({"intro": lambda x: "。".join(set(x))})
+    intro_merged_each_link["tf_idf"] = intro_merged_each_link.intro.apply(lambda x: jieba.analyse.tfidf(x, topK=None, withWeight=True))
+    intro_merged_each_link["textrank"] = intro_merged_each_link.intro.apply(lambda x: jieba.analyse.textrank(x, topK=None, withWeight=True))
+    intro_merged_each_link.tf_idf = intro_merged_each_link.tf_idf.apply(lambda x: dict(x))
+    intro_merged_each_link.textrank = intro_merged_each_link.textrank.apply(lambda x: dict(x))
+    
+    tfidf_result_dict = dict(zip(intro_merged_each_link.index, intro_merged_each_link.tf_idf))
+    textrank_result_dict = dict(zip(intro_merged_each_link.index, intro_merged_each_link.textrank))
+    
+    tfidf_bucket_file_name = "../Data/Output/word_buckets/tfidf_bucket_lv%d.pkl" % bucket_level
+    textrank_bucket_file_name = "../Data/Output/word_buckets/textrank_bucket_lv%d.pkl" % bucket_level
+    
+    tfidf_bucket_file = open(tfidf_bucket_file_name, "wb")
+    pickle.dump(tfidf_result_dict, tfidf_bucket_file)
+    tfidf_bucket_file.close()
+    
+    textrank_bucket_file = open(textrank_bucket_file_name, "wb")
+    pickle.dump(textrank_result_dict, textrank_bucket_file)
+    textrank_bucket_file.close()
+    
+    return (tfidf_result_dict, textrank_result_dict)
+
+
+def bucket_by_text_textrank(bucket_level, comp_ctaglinks, comp_intro_merged):
+    level_groups = comp_ctaglinks.groupby("level")
+    comp_link = level_groups.get_group(bucket_level)
+    comp_link_with_all_intros = comp_link.merge(comp_intro_merged, how="left", left_on="comp_id", right_on="comp_id")
+    intro_merged_each_link = comp_link_with_all_intros.groupby("link").agg({"intro": lambda x: "。".join(set(x))})
+    intro_merged_each_link["textrank"] = intro_merged_each_link.intro.apply(lambda x: jieba.analyse.tfidf(x, topK=None, withWeight=True))
     return
+
+def load_bucket(path):
+    return pickle.load(open(path, "rb"))
+
+def tagger(comp_word_list, bucket, top=5):
+    link_list = list(bucket.keys())
+    scores = [sum([bucket.get(link).get(word, 0) for word in comp_word_list]) for link in link_list]
+    result = pd.DataFrame({"link_name": link_list, "score": scores})
+    return result.sort_values(by="score", ascending=False)[:top]
+
+def tagger_for_df(intro_df, bucket_list, col_name="words", top=5):
+    if col_name not in intro_df:
+        print("'words' column not found in dataframe!")
+        return -1
+    for name, bucket in bucket_list.items():
+        intro_df[name] = intro_df.words.apply(lambda x: tagger(x, bucket, top=top).values)
+    return intro_df
+    
+def multi_process_tagger(intro_df, bucket_list, col_name="words", top=5, process_num=8):
+    if col_name not in intro_df:
+        print("'words' column not found in dataframe!")
+        return -1
+    result_list = []
+    split_intro_infos = np.array_split(intro_df, process_num)
+    pool = mp.Pool()
+    for i in range(0, process_num):
+        result_list.append(pool.apply_async(tagger_for_df, (split_intro_infos[i], bucket_list,)))
+    pool.close()
+    pool.join()
+    result_merged = pd.concat([r.get() for r in result_list])
+    return result_merged
 
 def bucket_generator(bucket):
     bucket_by_tag()
